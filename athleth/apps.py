b7cc79bae07f582eee8c8971b05466339aa13793
MuzzammilKhan-creator <sadrmozzamilkhan@gmail.com>
2025-07-08 22:22:22 -0700

    Initial commit for sec project

---
+++ athleth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AthlethConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'athleth'
---
+++ nima_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nima_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nima_project.settings')
+
+application = get_wsgi_application()
